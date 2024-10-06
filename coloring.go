e1c8c1a468668eea99df389f45fefa9ffc4a80aa
oscarracuna <acunainfosec@proton.me>
2024-10-06 17:51:42 -0600

    init

--- coloring.go
+++ coloring.go
@@ -1,9 +1,14 @@
-package main
+package coloring
 
-import (
-  "fmt"
+var (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+	Bold   = "\033[1m"
 )
-
-func main() {
-  fmt.Println("ola")
-}
